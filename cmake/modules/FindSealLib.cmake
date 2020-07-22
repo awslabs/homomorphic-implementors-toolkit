a927553aece64f4dcf6370cee113799f54e72195
Bryce Shang <65570357+bryce-shang@users.noreply.github.com>
2020-07-22 09:36:56 -0700

    Import Microsoft SEAL using git subtree (#3).
    * Add git subtree doc link.
    
    * Squashed 'third-party/seal/src/' content from commit f7d748c
    
    git-subtree-dir: third-party/seal/src
    git-subtree-split: f7d748c97ed841376c4a1cdec9e7c978f5e64a95

--- cmake/modules/FindSealLib.cmake
+++ cmake/modules/FindSealLib.cmake
@@ -6,4 +6,5 @@ message(STATUS "Searching Microsoft SEAL.")
 # SEAL is a `git subtree` of this repository because some tweaks are made based on our needs.
 # The subtree is under ${HIT_THIRD_PARTY_DIR}/seal/src
 # These tweaks may be contributed back to SEAL git repo by opening issues.
+# https://github.github.com/training-kit/downloads/submodule-vs-subtree-cheat-sheet/
 add_subdirectory(${HIT_THIRD_PARTY_DIR}/seal/src)
