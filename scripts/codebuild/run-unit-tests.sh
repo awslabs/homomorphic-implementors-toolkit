fb62466da29e2c4a070d0ffbf6482958b2e19c34
Bryce Shang <shanggu@amazon.com>
2020-08-20 15:11:37 -0700

    Add configure to find glog lib.
    Use Glog.

--- scripts/codebuild/run-unit-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-if [ -d build ]; then rm -Rf build; fi
-mkdir -p build && cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMake_RUN_CLANG_TIDY=ON -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON ../
-ninja -j $(nproc)
-ninja run_evaldemo
