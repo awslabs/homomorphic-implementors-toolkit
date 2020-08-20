850f78a36fed49e209bc760f2cd93a4ae83cb9ff
Eric Crockett <ecrockett0@gmail.com>
2020-08-20 14:45:05 -0700

    Added API docs

--- src/api/evaluator.h
+++ src/api/evaluator.h
@@ -48,16 +48,24 @@ namespace hit {
          */
         CKKSCiphertext rotate_right(const CKKSCiphertext &ct, int steps);
 
+        /* Rotate a plaintext vector cyclically to the right, inplace.
+         */
         void rotate_right_inplace(CKKSCiphertext &ct, int steps);
 
         /* Rotate a plaintext vector cyclically to the left.
          */
         CKKSCiphertext rotate_left(const CKKSCiphertext &ct, int steps);
 
+        /* Rotate a plaintext vector cyclically to the left, inplace.
+         */
         void rotate_left_inplace(CKKSCiphertext &ct, int steps);
 
+        /* Negate each coefficient of a plaintext vector.
+         */
         CKKSCiphertext negate(const CKKSCiphertext &ct);
 
+        /* Negate each coefficient of a plaintext vector, inplace.
+         */
         void negate_inplace(CKKSCiphertext &ct);
 
         /* Add a scalar to (each slot of) the ciphertext, and place the result in `dest`.
@@ -65,38 +73,69 @@ namespace hit {
          */
         CKKSCiphertext add_plain(const CKKSCiphertext &ct, double scalar);
 
+        /* Add a scalar to (each slot of) the ciphertext, and place the result in the first argument.
+         * The plaintext is encoded with the same scale as the ciphertext.
+         */
         void add_plain_inplace(CKKSCiphertext &ct, double scalar);
 
+        /* Add a plaintext component-wise to an encrypted plaintext.
+         */
         CKKSCiphertext add_plain(const CKKSCiphertext &ct, const std::vector<double> &plain);
 
+        /* Add a plaintext component-wise to an encrypted plaintext, inplace.
+         */
         void add_plain_inplace(CKKSCiphertext &ct, const std::vector<double> &plain);
+
+        /* Add two ciphertexts (inducing component-wise addition on plaintexts)
+         * and store the result in a new ciphertext.
+         */
+        CKKSCiphertext add(const CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
 
         /* Add two ciphertexts (inducing component-wise addition on plaintexts)
          * and store the result in the first parameter.
          */
-        CKKSCiphertext add(const CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
-
         void add_inplace(CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
 
+        /* Add the ciphertexts in a vector. The vector must be non-empty.
+         */
         CKKSCiphertext add_many(std::vector<CKKSCiphertext> &cts);
 
+        /* Subtract two ciphertexts (inducing component-wise subtraction on plaintexts)
+         * and store the result in a new ciphertext.
+         */
         CKKSCiphertext sub(const CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
 
+        /* Subtract two ciphertexts (inducing component-wise subtraction on plaintexts)
+         * and store the result in the first parameter.
+         */
         void sub_inplace(CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
 
+        /* Subtract a scalar from (each slot of) the ciphertext, and place the result in `dest`.
+         * The plaintext is encoded with the same scale as the ciphertext.
+         */
         CKKSCiphertext sub_plain(const CKKSCiphertext &ct, double scalar);
 
+        /* Subtract a scalar from (each slot of) the ciphertext, and place the result in the first argument.
+         * The plaintext is encoded with the same scale as the ciphertext.
+         */
         void sub_plain_inplace(CKKSCiphertext &ct, double scalar);
 
+        /* Subtract a plaintext component-wise from an encrypted plaintext.
+         */
         CKKSCiphertext sub_plain(const CKKSCiphertext &ct, const std::vector<double> &plain);
 
+        /* Subtract a plaintext component-wise from an encrypted plaintext, inplace.
+         */
         void sub_plain_inplace(CKKSCiphertext &ct, const std::vector<double> &plain);
+
+        /* Multiply two ciphertexts (inducing component-wise multiplication on
+         * plaintexts) and store the result in the a new ciphertext.
+         */
+        CKKSCiphertext multiply(const CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
 
         /* Multiply two ciphertexts (inducing component-wise multiplication on
          * plaintexts) and store the result in the first parameter.
          */
-        CKKSCiphertext multiply(const CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
-
         void multiply_inplace(CKKSCiphertext &ct1, const CKKSCiphertext &ct2);
 
         /* Multiply the ciphertext by the plaintext, and store the result in `dest`.
@@ -107,6 +146,12 @@ namespace hit {
          */
         CKKSCiphertext multiply_plain(const CKKSCiphertext &ct, double scalar);
 
+        /* Multiply the ciphertext by the plaintext, and store the result in the first parameter.
+         * The plaintext is encoded using the same scale as the ciphertext.
+         *
+         * WARNING: Multiplying by 0 results in non-constant time behavior! Only multiply by 0 if the scalar is truly
+         * public.
+         */
         void multiply_plain_inplace(CKKSCiphertext &ct, double scalar);
 
         /* Multiply the ciphertext by the plaintext. This API is different than the corresponding SEAL API:
@@ -116,22 +161,29 @@ namespace hit {
          */
         CKKSCiphertext multiply_plain(const CKKSCiphertext &ct, const std::vector<double> &plain);
 
+        /* Multiply the ciphertext by the plaintext, inplace. This API is different than the corresponding SEAL API:
+         * it takes a C++ vector whose size is the same as the size of the plaintext encrypted by the ciphertext,
+         * and is interpreted as a matrix (i.e., no linear algebra encoding is performed).
+         * The plaintext is encoded using the same scale as the ciphertext.
+         */
         void multiply_plain_inplace(CKKSCiphertext &ct, const std::vector<double> &plain);
 
-        /* Multiply the first input by itself, and store the result in the second
-         * parameter.
+        /* Multiply the first input by itself, and store the result in a new ciphertext.
          */
         CKKSCiphertext square(const CKKSCiphertext &ct);
 
+        /* Square the input inplace.
+         */
         void square_inplace(CKKSCiphertext &ct);
 
         CKKSCiphertext mod_down_to(const CKKSCiphertext &ct, const CKKSCiphertext &target);
 
-        /* Reduce the HE level of `x` to the level of the `target.
+        /* Reduce the HE level of `x` to the level of the `target`, inplace.
          */
         void mod_down_to_inplace(CKKSCiphertext &ct, const CKKSCiphertext &target);
 
         /* Reduce the HE level of both inputs to the lower of the two levels.
+         * This can modify at most one of the inputs.
          */
         void mod_down_to_min_inplace(CKKSCiphertext &ct1, CKKSCiphertext &ct2);
 
@@ -140,12 +192,18 @@ namespace hit {
          */
         CKKSCiphertext mod_down_to_level(const CKKSCiphertext &ct, int level);
 
+        /* Reduce the HE level of `x` to level `level`, which has
+         * level+1 moduli. `level` must be >= 0. Store the result in the first arugment.
+         */
         void mod_down_to_level_inplace(CKKSCiphertext &ct, int level);
-
-        CKKSCiphertext rescale_to_next(const CKKSCiphertext &ct);
 
         /* Remove a prime from the modulus (i.e. go down one level) and scale
          * down the plaintext by that prime.
+         */
+        CKKSCiphertext rescale_to_next(const CKKSCiphertext &ct);
+
+        /* Remove a prime from the modulus (i.e. go down one level) and scale
+         * down the plaintext by that prime. Store the result inplace.
          */
         void rescale_to_next_inplace(CKKSCiphertext &ct);
 
