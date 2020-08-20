0e8611ed736b34052504eef757eae28e050a7163
Bryce Shang <shanggu@amazon.com>
2020-08-20 14:31:00 -0700

    Turn on clang-tidy: WarningsAsErrors.

--- scripts/codebuild/run_unit_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-if [ -d build ]; then rm -Rf build; fi
-cmake . -Bbuild/Release -GNinja -DCMAKE_BUILD_TYPE=Release
-ninja -Cbuild/Release
