7b709b06d990ae6865b47949a77011dc5059408f
Eric Crockett <ecrockett0@gmail.com>
2020-08-21 15:39:30 -0700

    Reorganized CMakeLists structure to decouple as much as possible. Also renamed library from hit to aws-hit. This results in a library named libaws-hit.a

--- scripts/codebuild/run-unit-tests.sh
+++ scripts/codebuild/run-unit-tests.sh
@@ -4,5 +4,6 @@
 
 if [ -d build ]; then rm -Rf build; fi
 mkdir -p build && cd build
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMake_RUN_CLANG_TIDY=ON ../
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMake_RUN_CLANG_TIDY=ON -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON../
 ninja -j $(nproc)
+ninja run_evaldemo
