37b0dc9d3e861d892bb2b9a77df5eeeddc0bf749
Eric Crockett <ecrockett0@gmail.com>
2020-08-24 09:02:25 -0700

    Fixed a bug in depthfinder when using explicit encryption levels

--- tests/api/evaluator/depthfinder.cpp
+++ tests/api/evaluator/depthfinder.cpp
@@ -247,3 +247,13 @@ TEST(DepthFinderTest, RescaleToNextInPlace) {
     ASSERT_EQ(he_level - 1, ciphertext1.he_level);
     ASSERT_EQ(1, ckksInstance->get_multiplicative_depth());
 }
+
+TEST(DepthFinderTest, RescaleToNextInPlace_ExplicitLevel) {
+    CKKSInstance *ckksInstance = CKKSInstance::get_new_depthfinder_instance(VERBOSE);
+    CKKSCiphertext ciphertext1;
+    ckksInstance->encrypt_row_vec(VECTOR_1, SIZE, ciphertext1, 1);
+    int he_level = ciphertext1.he_level;
+    ckksInstance->evaluator->rescale_to_next_inplace(ciphertext1);
+    ASSERT_EQ(he_level - 1, ciphertext1.he_level);
+    ASSERT_EQ(1, ckksInstance->get_multiplicative_depth());
+}
