7999873fa84fb2d60a63567813973666a780d24c
Bryce Shang <shanggu@amazon.com>
2020-08-27 23:45:07 -0700

    Avoid build when glog is system installed.

--- cmake/modules/FindGlogLib.cmake
+++ cmake/modules/FindGlogLib.cmake
@@ -2,10 +2,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # https://github.com/google/glog/blob/master/cmake/INSTALL.md
-message(STATUS "Downloading and installing Glog.")
-download_external_project("glog")
-set(WITH_GFLAGS OFF CACHE BOOL "Disable optional build flavor -- WITH_GFLAGS.")
-add_subdirectory(
-        ${HIT_THIRD_PARTY_DIR}/glog/src
-        ${HIT_THIRD_PARTY_DIR}/glog/build
-        EXCLUDE_FROM_ALL)
+message(STATUS "Searching glog lib.")
+find_package(glog 0.3.5)
+if (glog_FOUND)
+    message(STATUS "Glog is found. Skip downloading source code.")
+else ()
+    message(STATUS "Downloading and installing Glog since it is not found.")
+    download_external_project("glog")
+    set(WITH_GFLAGS OFF CACHE BOOL "Disable optional build flavor -- WITH_GFLAGS.")
+    add_subdirectory(
+            ${HIT_THIRD_PARTY_DIR}/glog/src
+            ${HIT_THIRD_PARTY_DIR}/glog/build
+            EXCLUDE_FROM_ALL)
+endif ()
