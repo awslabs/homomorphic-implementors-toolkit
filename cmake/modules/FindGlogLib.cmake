11a8cadc055163b6b1cb56257fb2467c26917212
Bryce Shang <shanggu@amazon.com>
2020-08-27 23:41:02 -0700

    Fix glog build and related compiling issues.

--- cmake/modules/FindGlogLib.cmake
+++ cmake/modules/FindGlogLib.cmake
@@ -1,8 +1,10 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# https://github.com/google/glog/blob/master/cmake/INSTALL.md
 message(STATUS "Downloading and installing Glog.")
 download_external_project("glog")
+set(WITH_GFLAGS OFF CACHE BOOL "Disable optional build flavor -- WITH_GFLAGS.")
 add_subdirectory(
         ${HIT_THIRD_PARTY_DIR}/glog/src
         ${HIT_THIRD_PARTY_DIR}/glog/build
--- src/hit/sealutils.h
+++ src/hit/sealutils.h
@@ -5,7 +5,6 @@
 
 #include <iomanip>
 #include <iostream>
-#include <glog/logging.h>
 
 #include "seal/seal.h"
 
@@ -15,88 +14,17 @@ namespace hit {
 
     Copied from SEAL ./native/examples/examples.h
     */
-    inline void print_parameters(const std::shared_ptr<seal::SEALContext> &context) {
-        // Verify parameters
-        if (!context) {
-            throw std::invalid_argument("context is not set");
-        }
-        const auto &context_data = *context->key_context_data();
-
-        /*
-        Which scheme are we using?
-        */
-        std::string scheme_name;
-        switch (context_data.parms().scheme()) {
-            case seal::scheme_type::CKKS:
-                scheme_name = "CKKS";
-                break;
-            default:
-                throw std::invalid_argument("unsupported scheme");
-        }
-        LOG(INFO) << "/";
-        LOG(INFO) << "| Encryption parameters :";
-        LOG(INFO) << "|   scheme: " << scheme_name;
-        LOG(INFO) << "|   poly_modulus_degree: " << context_data.parms().poly_modulus_degree();
-
-        /*
-        Print the size of the true (product) coefficient modulus.
-        */
-        std::stringstream coeff_modulus_size_info;
-        coeff_modulus_size_info << "|   coeff_modulus size: ";
-        coeff_modulus_size_info << context_data.total_coeff_modulus_bit_count() << " (";
-        auto coeff_modulus = context_data.parms().coeff_modulus();
-        std::size_t coeff_modulus_size = coeff_modulus.size();
-        for (std::size_t i = 0; i < coeff_modulus_size - 1; i++) {
-            coeff_modulus_size_info << coeff_modulus[i].bit_count() << " + ";
-        }
-        coeff_modulus_size_info << coeff_modulus.back().bit_count();
-        coeff_modulus_size_info << ") bits";
-        LOG(INFO) << coeff_modulus_size_info.str();
-
-        /*
-        For the BFV scheme print the plain_modulus parameter.
-        */
-        if (context_data.parms().scheme() == seal::scheme_type::BFV) {
-            LOG(INFO) << "|   plain_modulus: " << context_data.parms().plain_modulus().value();
-        }
-
-        LOG(INFO) << "\\";
-    }
+    void print_parameters(const std::shared_ptr<seal::SEALContext> &context);
 
     /*
     Helper function: Prints the `parms_id' to std::ostream.
 
     Copied from SEAL ./native/examples/examples.h
     */
-    inline std::ostream &operator<<(std::ostream &stream, seal::parms_id_type parms_id) {
-        /*
-        Save the formatting information for std::cout.
-        */
-        std::ios old_fmt(nullptr);
-        old_fmt.copyfmt(std::cout);
-
-        stream << std::hex << std::setfill('0') << std::setw(16) << parms_id[0] << " " << std::setw(16) << parms_id[1]
-               << " " << std::setw(16) << parms_id[2] << " " << std::setw(16) << parms_id[3] << " ";
-
-        /*
-        Restore the old std::cout formatting.
-        */
-        std::cout.copyfmt(old_fmt);
-
-        return stream;
-    }
+    std::ostream &operator<<(std::ostream &stream, seal::parms_id_type parms_id);
 
     /*
     Helper function: Fetch the last prime given SEALContext and heLevel.
     */
-    inline std::uint64_t getLastPrime(const std::shared_ptr<seal::SEALContext> &context, const int heLevel) {
-        auto context_data = context->first_context_data();
-        while (context_data->chain_index() >= heLevel) {
-            if (context_data->chain_index() == heLevel) {
-                return context_data->parms().coeff_modulus().back().value();
-            }
-            context_data = context_data->next_context_data();
-        }
-        throw std::invalid_argument("Fail to find target level " + std::to_string(heLevel));
-    }
+    std::uint64_t getLastPrime(const std::shared_ptr<seal::SEALContext> &context, int heLevel);
 }  // namespace hit
