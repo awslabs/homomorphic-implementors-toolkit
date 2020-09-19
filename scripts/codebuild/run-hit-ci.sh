4f55810c0ad40f56308555231013798a408a279f
Eric Crockett <ecrockett0@gmail.com>
2020-09-19 07:53:42 -0700

    Disabled auto-running the tests. Updated all CMake flags for consistent formatting. Added a custom build target to runt the tests. Updated the README.

--- scripts/codebuild/run-hit-ci.sh
+++ scripts/codebuild/run-hit-ci.sh
@@ -21,7 +21,8 @@ mkdir -p ${GLOG_log_dir}
 if [ -d build ]; then rm -Rf build; fi
 mkdir -p build && cd build
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_HIT_TESTING=ON -DCMake_RUN_CLANG_TIDY=ON \
-    -DBUILD_HIT_EXAMPLES=ON ../
+    -DHIT_BUILD_TESTS=ON -DHIT_RUN_CLANG_TIDY=ON \
+    -DHIT_BUILD_EXAMPLES=ON ../
 ninja -j $(nproc)
+ninja run_hit_tests
 ninja run_hit_example
