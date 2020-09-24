d6866a19b5b0cfb8dc0ae9bb04b40650e9616d78
Eric Crockett <ecrockett0@gmail.com>
2020-09-24 15:49:30 -0700

    Added a top-level export header

---
+++ src/hit/hit.h
@@ -0,0 +1,21 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// This file includes most of the headers that are typically used in an application.
+
+#include "hit/api/linearalgebra/linearalgebra.h"
+#include "hit/api/linearalgebra/encodingunit.h"
+#include "hit/api/linearalgebra/encryptedmatrix.h"
+#include "hit/api/linearalgebra/encryptedrowvector.h"
+#include "hit/api/linearalgebra/encryptedcolvector.h"
+#include "hit/api/evaluator/debug.h"
+#include "hit/api/evaluator/depthfinder.h"
+#include "hit/api/evaluator/homomorphic.h"
+#include "hit/api/evaluator/opcount.h"
+#include "hit/api/evaluator/plaintext.h"
+#include "hit/api/evaluator/scaleestimator.h"
+#include "hit/api/ciphertext.h"
+#include "hit/api/evaluator.h"
+#include "hit/common.h"
