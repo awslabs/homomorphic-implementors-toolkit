5e4d1263427124cd6b66a6fc746acd78267caf79
Eric Crockett <ecrockett0@gmail.com>
2020-10-13 13:06:35 -0700

    Converted glog

--- cmake/modules/FindGlogLib.cmake
+++ cmake/modules/FindGlogLib.cmake
@@ -10,8 +10,5 @@ else ()
     message(STATUS "Downloading and installing Glog since it is not found.")
     download_external_project("glog")
     set(WITH_GFLAGS OFF CACHE BOOL "Disable optional build flavor -- WITH_GFLAGS.")
-    add_subdirectory(
-            ${HIT_THIRD_PARTY_DIR}/glog/src
-            ${HIT_THIRD_PARTY_DIR}/glog/build
-            EXCLUDE_FROM_ALL)
+    find_package(glog REQUIRED)
 endif ()
