bb093fece969e2c647701202b603178c210a25ef
Eric Crockett <ecrockett0@gmail.com>
2020-10-14 16:22:01 -0700

    More fixes to protbuf installation and suppressed output of download step

--- cmake/download-external-project.cmake
+++ cmake/download-external-project.cmake
@@ -2,17 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
 function(download_external_project project_dir)
-    message(STATUS "Downloading ${project_dir}...")
     set(EXTERNAL_PROJECT_CMAKE_CACHE_FILE ${HIT_THIRD_PARTY_DIR}/${project_dir}/CMakeCache.txt)
     if(EXISTS ${EXTERNAL_PROJECT_CMAKE_CACHE_FILE})
         message(STATUS "Removing old ${EXTERNAL_PROJECT_CMAKE_CACHE_FILE}")
         file(REMOVE ${EXTERNAL_PROJECT_CMAKE_CACHE_FILE})
     endif()
     set(COMMAND_WORK_DIR ${HIT_THIRD_PARTY_DIR}/${project_dir})
+    message(STATUS "Downloading ${project_dir}...")
     execute_process(
         COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . -D3P_INSTALL_DIR=${3P_INSTALL_DIR}
         RESULT_VARIABLE result
-        WORKING_DIRECTORY ${COMMAND_WORK_DIR})
+        WORKING_DIRECTORY ${COMMAND_WORK_DIR}
+        OUTPUT_QUIET)
     if(result)
         message(FATAL_ERROR "Failed to download (${result}).")
     endif()
--- cmake/modules/FindProtobufLib.cmake
+++ cmake/modules/FindProtobufLib.cmake
@@ -6,12 +6,16 @@ find_program(Protoc_FOUND protoc)
 # require both the protobuf library and the protoc compiler to be installed,
 # otherwise build them ourselves. The versions must be compatible,
 # so it's not a good idea to use, e.g., a system-installed protoc and
-# build-from-source protobuf library\
+# build-from-source protobuf library
 if (Protobuf_FOUND AND Protoc_FOUND)
     message(STATUS "Protobuf is already installed.")
 else ()
     message(STATUS "Protobuf was not found on your system.")
     download_external_project("protobuf")
-    find_package(Protobuf "3.0.0" REQUIRED)
+    # Even though find_package(Protobuf) succeeds at this point, it doesn't set
+    # the variables `Protobuf_LIBRARIES` and `Protobuf_PROTOC_EXECUTABLE`
+    # like it is supposed to, so we set them manually.
+    # find_package(Protobuf "3.0.0" REQUIRED HINTS ${3P_INSTALL_DIR})
     set(Protobuf_LIBRARIES "${3P_INSTALL_DIR}/lib/libprotobuf.a")
+    find_program(Protobuf_PROTOC_EXECUTABLE protoc)
 endif ()
