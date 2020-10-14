614622cacaff6a1b3ec9ae6cb2e2ac55128065f2
Eric Crockett <ecrockett0@gmail.com>
2020-10-14 15:35:56 -0700

    minimize diff

--- scripts/codebuild/docker/build_ci_images.sh
+++ scripts/codebuild/docker/build_ci_images.sh
@@ -14,13 +14,12 @@ $(aws ecr get-login --no-include-email --region us-west-2)
 
 docker build -f ubuntu-18.04_base.docker -t ubuntu-18.04:base .
 docker tag ubuntu-18.04:base ${ECS_REPO}:ubuntu-18.04_base
+docker push ${ECS_REPO}:ubuntu-18.04_base
 
 docker build -f ubuntu-18.04_gcc-9x.docker -t ubuntu-18.04:gcc-9x .
 docker tag ubuntu-18.04:gcc-9x ${ECS_REPO}:ubuntu-18.04_gcc-9x
+docker push ${ECS_REPO}:ubuntu-18.04_gcc-9x
 
 docker build -f ubuntu-18.04_clang-10x.docker -t ubuntu-18.04:clang-10x .
 docker tag ubuntu-18.04:clang-10x ${ECS_REPO}:ubuntu-18.04_clang-10x
-
-docker push ${ECS_REPO}:ubuntu-18.04_base
-docker push ${ECS_REPO}:ubuntu-18.04_gcc-9x
 docker push ${ECS_REPO}:ubuntu-18.04_clang-10x
