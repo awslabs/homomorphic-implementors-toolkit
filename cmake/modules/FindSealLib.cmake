34f20bc30bffa6f24979dacdce47923b012d8d83
Eric Crockett <ecrockett0@gmail.com>
2020-12-29 16:52:20 -0800

    Added conditional download to all dependencies

--- cmake/modules/FindSealLib.cmake
+++ cmake/modules/FindSealLib.cmake
@@ -2,20 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Download and install dependency - Microsoft SEAL.
-message(STATUS "3P_INSTALL_DIR ${3P_INSTALL_DIR}")
-set(SEAL_DIR ${3P_INSTALL_DIR})
-#set(SEAL_DIR ${CMAKE_SOURCE_DIR}/external/hit/build/Release/third-party/lib/cmake/SEAL-3.6/)
-set(SEAL_DIR ${CMAKE_SOURCE_DIR}/build/Release/external/hit/third-party/lib/cmake/SEAL-3.6/)
-find_package(SEAL 3.6 QUIET)
-
-if (NOT SEAL_FOUND)
-	download_external_project("seal")
-	find_package(SEAL 3.6 REQUIRED)
-
-	if (NOT SEAL_FOUND)
-		message(FATAL "SEAL NOT FOUND")
-	endif()
-endif()
-
-# when building HELR: 3P_INSTALL_DIR /home/ubuntu/HELR/build/Release/external/hit/third-party
-# when building HIT:  3P_INSTALL_DIR /home/ubuntu/HELR/external/hit/build/Release/third-party
+download_external_project("seal")
+find_package(SEAL 3.6 REQUIRED)
