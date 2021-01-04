3685f4c30eb4021aa15947c78efecc4407b10944
Eric Crockett <ecrockett0@gmail.com>
2021-01-04 14:37:09 -0800

    Don't redownload dependencies if they are already present, and fix missing include directory for gtest

--- cmake/download-external-project.cmake
+++ cmake/download-external-project.cmake
@@ -2,13 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 function(download_external_project project_dir)
-    set(EXTERNAL_PROJECT_CMAKE_CACHE_FILE ${HIT_THIRD_PARTY_DIR}/${project_dir}/CMakeCache.txt)
-    if(EXISTS ${EXTERNAL_PROJECT_CMAKE_CACHE_FILE})
-        message(STATUS "Removing old ${EXTERNAL_PROJECT_CMAKE_CACHE_FILE}")
-        file(REMOVE ${EXTERNAL_PROJECT_CMAKE_CACHE_FILE})
-    endif()
     set(COMMAND_WORK_DIR ${HIT_THIRD_PARTY_DIR}/${project_dir})
-    message(STATUS "Downloading ${project_dir}...")
+    message(STATUS "Cleaning ${project_dir} directory...")
+    execute_process(
+        # delete all items in ${COMMAND_WORK_DIR} _execpt_
+        #  - CMakeLists.txt (which is part of the repo)
+        #  - src/, which is where our build system unpacks the downloaded content. If this folder exists, we don't need to re-download
+        COMMAND find . -maxdepth 1 \( ! -name "CMakeLists.txt" ! -name "." ! -path "./src" \) -exec rm -rf {} \;
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${COMMAND_WORK_DIR}
+        OUTPUT_QUIET)
+    message(STATUS "Obtaining ${project_dir}...")
     execute_process(
         COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . -D3P_INSTALL_DIR=${3P_INSTALL_DIR}
         RESULT_VARIABLE result
--- cmake/set-common-flags.cmake
+++ cmake/set-common-flags.cmake
@@ -2,13 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 function(set_common_flags target_name)
-    target_compile_options(${target_name} PUBLIC -std=c++17 -Wall -Werror -Wformat=2 -Wwrite-strings -Wvla
+    target_compile_options(${target_name} PRIVATE -std=c++17 -Wall -Werror -Wformat=2 -Wwrite-strings -Wvla
             -fvisibility=hidden -fno-common -funsigned-char -Wextra -Wunused -Wcomment -Wchar-subscripts -Wuninitialized
             -Wunused-result -Wfatal-errors)
     if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        target_compile_options(${target_name} PUBLIC -Wmissing-declarations -Wmissing-field-initializers -Wshadow
+        target_compile_options(${target_name} PRIVATE -Wmissing-declarations -Wmissing-field-initializers -Wshadow
                 -Wpedantic)
     endif()
     #-Wextra turns on sign-compare which is strict on comparing loop indexes (int) with size_t from vector length
-    target_compile_options(${target_name} PUBLIC -Wno-sign-compare)
+    target_compile_options(${target_name} PRIVATE -Wno-sign-compare)
 endfunction()
