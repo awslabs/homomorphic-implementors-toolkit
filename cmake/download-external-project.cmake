2a03d61dd914cbf594a4c7b8ce1be6b1a8c4c167
Greg Rubin <rubin@amazon.com>
2021-07-09 20:41:44 +0000

    Pull all dependencies from git and don't always rebuild

--- cmake/download-external-project.cmake
+++ cmake/download-external-project.cmake
@@ -1,17 +1,21 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+option(HIT_REBUILD_DEPS "Rebuild dependendies" OFF)
+
 function(download_external_project project_dir)
     set(COMMAND_WORK_DIR ${HIT_THIRD_PARTY_DIR}/${project_dir})
-    message(STATUS "Cleaning ${project_dir} directory...")
-    execute_process(
-        # delete all items in ${COMMAND_WORK_DIR} _execpt_
-        #  - CMakeLists.txt (which is part of the repo)
-        #  - src/, which is where our build system unpacks the downloaded content. If this folder exists, we don't need to re-download
-        COMMAND find . -maxdepth 1 \( ! -name "CMakeLists.txt" ! -name "." ! -path "./src" \) -exec rm -rf {} \;
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${COMMAND_WORK_DIR}
-        OUTPUT_QUIET)
+    if (HIT_REBUILD_DEPS)
+        message(STATUS "Cleaning ${project_dir} directory...")
+        execute_process(
+            # delete all items in ${COMMAND_WORK_DIR} _execpt_
+            #  - CMakeLists.txt (which is part of the repo)
+            #  - src/, which is where our build system unpacks the downloaded content. If this folder exists, we don't need to re-download
+            COMMAND find . -maxdepth 1 \( ! -name "CMakeLists.txt" ! -name "." ! -path "./src" \) -exec rm -rf {} \;
+            RESULT_VARIABLE result
+            WORKING_DIRECTORY ${COMMAND_WORK_DIR}
+            OUTPUT_QUIET)
+    endif()
     message(STATUS "Obtaining ${project_dir}...")
     execute_process(
         COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . -D3P_INSTALL_DIR=${3P_INSTALL_DIR}
