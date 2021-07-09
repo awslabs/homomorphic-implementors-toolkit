f5994f4d306980080488441e3bbe8213775db1b7
Greg Rubin <rubin@amazon.com>
2021-07-09 20:01:37 +0000

    Pull TBB from git and build locally

--- cmake/modules/FindTbbLib.cmake
+++ cmake/modules/FindTbbLib.cmake
@@ -4,14 +4,10 @@
 download_external_project("tbb")
 
 # Load the TBB CMake file
-include(${HIT_THIRD_PARTY_DIR}/tbb/src/tbb/cmake/TBBGet.cmake)
-include(${HIT_THIRD_PARTY_DIR}/tbb/src/tbb/cmake/TBBMakeConfig.cmake)
+include(${HIT_THIRD_PARTY_DIR}/tbb/src/cmake/TBBBuild.cmake)
 message(STATUS "Installing TBB...")
-# Invoke it to automatically download the correct binary package for this system
-# Unpack the downloaded content to ${3P_INSTALL_DIR}
-# See https://sudonull.com/post/68014-Integration-of-Intel-Threading-Building-Blocks-into-your-CMake-project-Intel-Blog
-# for details on the tbb_get arguments.
-tbb_get(TBB_ROOT tbb_root RELEASE_TAG v2020.3 CONFIG_DIR TBB_DIR SAVE_TO ${3P_INSTALL_DIR})
+
+tbb_build(TBB_ROOT ${HIT_THIRD_PARTY_DIR}/tbb/src CONFIG_DIR TBB_DIR MAKE_ARGS)
 # Runtime linker error with GCC-9 in CI if I _don't_ specify the TBB components
 # The problem seems to be that the default is to use more components, but the linker
 # can't find the shared libraries of the (unused) components.
