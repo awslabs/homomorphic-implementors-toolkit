cbbdea47137f89a273e731d1e7409b116b21d79b
Eric Crockett <ecrockett0@gmail.com>
2021-08-06 10:03:18 -0700

    Removed extraneous print statements

--- src/hit/api/evaluator/debug.cpp
+++ src/hit/api/evaluator/debug.cpp
@@ -202,10 +202,6 @@ namespace hit {
             LOG(ERROR) << actual_debug_result.str();
 
             HomomorphicEval::PoolObject<Encoder> e = homomorphic_eval->get_encoder();
-            cout << "PT log_inf norm: " << log2(l_inf_norm(ct.raw_pt)) << endl;
-            cout << "log_scale: " << log2(ct.scale()) << endl;
-            cout << "helevel: " << ct.he_level() << endl;
-
             Plaintext encoded_plain =
                 encodeNTTAtLvlNew(homomorphic_eval->context->params, e.ref(), ct.raw_pt, ct.he_level(), ct.scale());
             vector<double> decoded_plain = ::decode(e.ref(), encoded_plain, log2(num_slots()));
