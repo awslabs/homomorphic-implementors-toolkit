e9a37a39fa93a68639ffa6b4b99e225e03dd2e16
Eric Crockett <ericcro@amazon.com>
2021-08-27 23:04:11 +0000

    Cleanup

--- src/hit/api/evaluator/homomorphic.h
+++ src/hit/api/evaluator/homomorphic.h
@@ -168,7 +168,6 @@ namespace hit {
         latticpp::RotationKeys galois_keys;
         latticpp::RelinearizationKey relin_keys;
         latticpp::BootstrappingKey btp_keys;
-        // int btp_depth = 0;
 
         PoolObject<latticpp::Evaluator> get_evaluator();
         PoolObject<latticpp::Encoder> get_encoder();
